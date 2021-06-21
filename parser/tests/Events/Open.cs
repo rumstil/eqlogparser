f9841175c4dfe791f32f84da28c5dca28d12a586
rumstil <raidloot@gmail.com>
2021-06-20 21:16:49 -0700

    Forgot to commit LogOpenEventTests

---
+++ parser/tests/Events/Open.cs
@@ -0,0 +1,30 @@
+﻿using EQLogParser;
+using Xunit;
+
+namespace EQLogParserTests.Event
+{
+    public class LogOpenEventTests
+    {
+        [Fact]
+        public void GetPlayerFromFileName()
+        {
+            // official format
+            Assert.Equal("Rumstil", LogOpenEvent.GetPlayerFromFileName("eqlog_Rumstil_erollisi.txt"));
+
+            // gzipped
+            Assert.Equal("Rumstil", LogOpenEvent.GetPlayerFromFileName("eqlog_Rumstil_erollisi.txt.gz"));
+
+            // in case we really want to be permissive with renamed files
+            Assert.Equal("Larry", LogOpenEvent.GetPlayerFromFileName("eqlog_Larry_erollisi-2020-07-05.txt"));
+            Assert.Equal("Curly", LogOpenEvent.GetPlayerFromFileName("eqlog_Curly-stuff.txt"));
+            Assert.Equal("Moe", LogOpenEvent.GetPlayerFromFileName("eqlog_Moe.txt"));
+        }
+
+        [Fact]
+        public void GetServerFromFileName()
+        {
+            Assert.Equal("erollisi", LogOpenEvent.GetServerFromFileName("eqlog_Rumstil_erollisi.txt"));
+        }
+
+    }
+}

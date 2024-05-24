bd9dbb4c2c48bd1ed62bf22149e338a520398a68
rumstil <raidloot@gmail.com>
2024-05-24 10:52:36 -0700

    Added helper function to check for log lines that may be missed by various parsers.

--- sample/Program.cs
+++ sample/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using EQLogParser;
 using EQLogParser.Helpers;
 
@@ -21,15 +23,47 @@ namespace Sample
     {
         static void Main(string[] args)
         {
+            //ScanLogAndShowUnhandledLines("/Proj/eq/logparser/logs/a/sample.txt");
+
+            ScanLogAndTrackFights("d:/games/everquest/logs/eqlog_Rumstil_erollisi.txt");
+        }
+
+        static void ScanLogAndShowUnhandledLines(string path)
+        {
+            var open = LogOpenEvent.FromFileName(path);
+
+            var parser = new LogParser();
+            parser.Player = open.Player ?? "Unknown";
+
+            var numbers = new Regex(@"\d+");
+
+            var reader = File.OpenText(open.Path);
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                    break;
+
+                var e = parser.ParseLine(line);
+
+                // show any unparsed lines that have a number in them
+                if (e is LogRawEvent raw && numbers.IsMatch(raw.Text))
+                    Console.WriteLine(raw);
+            };
+        }
+
+        static void ScanLogAndTrackFights(string path)
+        {
+
             // load spells to give the trackers more context when processing log files
             // this is optional and can be skipped
             Console.Error.WriteLine("Loading spells...");
             var spells = new SpellParser();
             spells.Load("d:/games/everquest/spells_us.txt");
-                
+
             // generate an open event that stores the player name, server name, and file path
             // all trackers should receive this as their first event to signal that a new log file is being processed
-            var open = LogOpenEvent.FromFileName("d:/games/everquest/logs/eqlog_Rumstil_erollisi.txt");
+            var open = LogOpenEvent.FromFileName(path);
 
             // create a CharTracker to help the FightTracker determine friends/foes
             var chars = new CharTracker(spells);
@@ -39,8 +73,8 @@ namespace Sample
             // create a FightTracker to build fight summaries from various combat events
             var fights = new FightTracker(spells, chars);
             fights.HandleEvent(open);
-            fights.OnFightStarted += ShowFight;
-            fights.OnFightFinished += ShowFight;
+            //fights.OnFightStarted += ShowFight;
+            //fights.OnFightFinished += ShowFight;
 
             // create a log parser for converting log lines into events that can be passed to the trackers
             var parser = new LogParser();
@@ -65,9 +99,9 @@ namespace Sample
                 // pass event to the trackers
                 chars.HandleEvent(e);
                 fights.HandleEvent(e);
-
-                //if (e is LogRawEvent) Console.WriteLine(e);
             };
+
+            //Console.WriteLine(chars.ExportPlayers());
 
             fights.ForceFightTimeouts();
             Console.Error.WriteLine("Parse completed in {0}", timer.Elapsed);
